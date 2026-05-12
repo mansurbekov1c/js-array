c8cf0649ac62dcbc5e3bdbf4c1fbbbfebf1b9d98
mansurbekov1c <abdullayevjorabek1pc@gmail.com>
2026-05-12 16:28:17 +0500

    get va put ishlatdik

--- back.js
+++ back.js
@@ -32,21 +32,93 @@
 //     });
 // });
 
-let btn = document.querySelector(".btn");
+// let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", (e) => {
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let title = document.querySelector("#title").value;
+//   let content = document.querySelector("#content").value;
+//   let author = document.querySelector("#author").value;
+
+//   fetch("https://axv-blogs.up.railway.app/api/posts", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       title,
+//       content,
+//       author,
+//     }),
+//   })
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// });
+
+// let btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let title = document.querySelector("#title").value;
+//   let content = document.querySelector("#content").value;
+//   // let author = document.querySelector("#author").value;
+
+//   fetch("https://axv-blogs.up.railway.app/api/posts/100", {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       title,
+//       content,
+//       // author,
+//     }),
+//   })
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// });
+
+let btn_get = document.querySelector(".btn_get");
+let btn_put = document.querySelector(".btn_put");
+
+btn_get.addEventListener("click", (e) => {
+  e.preventDefault();
+  let title = document.querySelector("#title");
+  let content = document.querySelector("#content");
+  let author = document.querySelector("#author");
+
+  fetch("https://axv-blogs.up.railway.app/api/posts/100", {
+    method: "GET",
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      title.value = data.title;
+      author.value = data.author;
+      content.value = data.content;
+
+      console.log(data);
+    });
+});
+
+btn_put.addEventListener("click", (e) => {
   e.preventDefault();
   let title = document.querySelector("#title").value;
   let content = document.querySelector("#content").value;
-  let author = document.querySelector("#author").value;
+  // let author = document.querySelector("#author").value;
 
-  fetch("https://axv-blogs.up.railway.app/api/posts", {
-    method: "POST",
+  fetch("https://axv-blogs.up.railway.app/api/posts/100", {
+    method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       title,
       content,
-      author,
+      // author,
     }),
   })
     .then(function (res) {
